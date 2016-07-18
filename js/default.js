3b97f01b950728e4e3073030c99d267aba532eee
o-shun <zyaooo99@yahoo.co.jp>
2016-07-18 13:41:26 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,15 @@
+var number = Math.floor(Math.random()*10);
+var answer = parseInt(window.prompt('数あてゲーム！０～９の数字を入力してください！'));
 
+var message;
+if(answer == number){
+  message = 'あたり！';
+}else if(answer < number){
+  message = '残念！もっと大きいです';
+}else if(number < answer){
+  message = '残念！もっと小さいです';
+}else{
+  message = '０～９の数字を入力してください';
+}
+
+document.getElementById('choice').textContent = message;
